504acbae4ce96407b3374b5f821b655cc3696337
thuatdev <phantruongthuatt@gmail.com>
2024-04-28 00:09:26 +0700

    feat:chat-bot

--- src/useRouteElements.tsx
+++ src/useRouteElements.tsx
@@ -1,11 +1,16 @@
 import { useRoutes } from 'react-router-dom'
 import Home from './components/Home/Home'
+import ChatBot from './components/ChatBot/ChatBox'
 
 export default function useRouteElements() {
   const routeElements = useRoutes([
     {
       path: '/',
       element: <Home />
+    },
+    {
+      path: '/chat',
+      element: <ChatBot />
     }
   ])
   return routeElements
