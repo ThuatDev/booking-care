515b44795ed036dab46cc72efcd5e2ca377a68a9
thuatdev <phantruongthuatt@gmail.com>
2024-04-27 16:34:28 +0700

    fix: ui-checkbox

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from 'src/App'
 import './index.css'
-// import './App.css'
 import { BrowserRouter } from 'react-router-dom'
+import dotenv from 'dotenv'
+dotenv.config()
+// import './App.css'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
